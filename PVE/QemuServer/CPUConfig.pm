aafa8c7947b305c2287db722f3d77c4a1d94ffde
jiangcuo <jiangcuo@bingsin.com>
2025-02-03 15:07:03 +0800

    Adjust some code so that it can pass the test on the x86_64 platform.

--- PVE/QemuServer/CPUConfig.pm
+++ PVE/QemuServer/CPUConfig.pm
@@ -213,7 +213,7 @@ my $cpu_fmt = {
 	    ." Only valid for custom CPU model definitions, default models will always report themselves to the guest OS.",
 	type => 'string',
 	enum => [ sort { lc("$a") cmp lc("$b") } keys %$cpu_vendor_list ],
-	default => 'max',
+	default => 'kvm64',
 	optional => 1,
     },
     hidden => {
@@ -794,15 +794,14 @@ sub get_default_cpu_type {
     my ($arch, $kvm) = @_;
 
     # if !kvm ,cpu will set to max for all arch
-    my $cputype = 'max';
-    if ($kvm){
-    $cputype = 'host' if $arch eq 'x86_64';
-    $cputype = 'host' if $arch eq 'aarch64';
-    $cputype = 'rv64' if $arch eq 'riscv64';
-    $cputype = 'la464' if $arch eq 'loongarch64';
-    }
+    my %cpu_map = (
+        'x86_64'      => $kvm ? 'kvm64' : 'qemu64',
+        'aarch64'     => $kvm ? 'host' : 'max',
+        'riscv64'     => $kvm ? 'rv64' : 'max',
+        'loongarch64' => $kvm ? 'la464' : 'max',
+    );
 
-    return $cputype;
+    return $cpu_map{$arch} // 'max';
 }
 
 sub is_native_arch($) {
