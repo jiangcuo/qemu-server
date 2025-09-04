74d439e10b3f84f9559820337126a4cf653cf0dd
Fiona Ebner <f.ebner@proxmox.com>
2025-09-04 14:40:52 +0200

    migration: preserve host_mtu for virtio-net devices
    The virtual hardware is generated differently (at least for i440fx
    machines) when host_mtu is set or not set on the netdev command line
    [0]. When the MTU is the same value as the default 1500, Proxmox VE
    did not add a host_mtu parameter. This is problematic for migration
    where host_mtu is present on one end of the migration, but not on the
    other [1]. Moreover, the effective setting in the guest (state) will
    still be the host_mtu from the source side, even if a different value
    is used for host_mtu on the target instance's commandline. This will
    not lead to an error loading the migration stream in QEMU, but having
    a larger host_mtu than the bridge MTU is still problematic for certain
    network traffic like
    > iperf3 -c 10.10.10.11 -u -l 2k
    when host_mtu=9000 and bridge MTU=1500.
    
    Pass the values from the source to the target during migration to be
    able to preserve them.
    
    [0]: https://bugzilla.redhat.com/show_bug.cgi?id=1449346
    [1]: https://forum.proxmox.com/threads/live-vm-migration-fails.169537/post-796379
    
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
    Reviewed-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
    Link: https://lore.proxmox.com/20250904124113.81772-9-f.ebner@proxmox.com

--- src/PVE/QemuServer.pm
+++ src/PVE/QemuServer.pm
@@ -9762,6 +9762,33 @@ sub delete_ifaces_ipams_ips {
     }
 }
 
+sub get_nets_host_mtu {
+    my ($vmid, $conf) = @_;
+
+    my $nets_host_mtu = [];
+    for my $opt (sort keys $conf->%*) {
+        next if $opt !~ m/^net(\d+)$/;
+        my $net = parse_net($conf->{$opt});
+        next if $net->{model} ne 'virtio';
+
+        my $host_mtu = eval {
+            mon_cmd(
+                $vmid, 'qom-get',
+                path => "/machine/peripheral/$opt",
+                property => 'host_mtu',
+            );
+        };
+        if (my $err = $@) {
+            log_warn("$opt: could not query host_mtu - $err");
+        } elsif (defined($host_mtu)) {
+            push $nets_host_mtu->@*, "${opt}=${host_mtu}";
+        } else {
+            log_warn("$opt: got undefined value when querying host_mtu");
+        }
+    }
+    return join(',', $nets_host_mtu->@*);
+}
+
 sub generate_vm_uuid {
     my ($vmid, $index) = @_;
     return sprintf("%08d-0000-0000-0000-%012d", $index, $vmid);
