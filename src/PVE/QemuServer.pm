c347f41fb0b736f9e2554f997e4dd74534082097
Fiona Ebner <f.ebner@proxmox.com>
2026-01-14 14:27:06 +0100

    vm status: schema: improve description for mem and memhost properties
    Suggested-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
    Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

--- src/PVE/QemuServer.pm
+++ src/PVE/QemuServer.pm
@@ -2557,7 +2557,8 @@ our $vmstatus_return_properties = {
         enum => ['stopped', 'running'],
     },
     mem => {
-        description => "Currently used memory in bytes.",
+        description => "Currently used memory in bytes. Does not take into account kernel"
+            . " same-page merging (KSM). Uses information from ballooning when available.",
         type => 'integer',
         optional => 1,
         renderer => 'bytes',
@@ -2569,7 +2570,8 @@ our $vmstatus_return_properties = {
         renderer => 'bytes',
     },
     memhost => {
-        description => "Current memory usage on the host.",
+        description => "Current memory usage on the host. Does not take into account kernel"
+            . " same-page merging (KSM).",
         type => 'integer',
         optional => 1,
         renderer => 'bytes',
